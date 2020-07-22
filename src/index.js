9d0cd1b9f0b5e3c9ebc899a33c25cbdf737da291
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2020-07-21 20:15:53 -0500

    chore: setup parcel

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <main>
+      <h1>Init project</h1>
+    </main>
+  );
+};
+
+const root = document.getElementById("app");
+ReactDOM.render(<App />, root);
